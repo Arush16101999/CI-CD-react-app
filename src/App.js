cc0d56e5d94797f8589eb5dc4978da95dcfe9043
arushmano <61136045+Arush16101999@users.noreply.github.com>
2023-12-21 23:35:47 +0530

    nice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavLink } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div
+          className="relative w-full max-w-[1490px] flex 
+        items-center justify-between pt-10 mx-auto px-10"
         >
-          Learn React
-        </a>
+          <ul className="hidden md:flex items-center gap-10 lg:gap-[68px]">
+            <NavLink to="services">Services</NavLink>
+            <NavLink to="products">Shop</NavLink>
+            <NavLink to="reference">Reference</NavLink>
+            <NavLink to="care">Care</NavLink>
+          </ul>
+        </div>
       </header>
     </div>
   );
